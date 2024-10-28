a6816e98b54a10c7fcf96832ae9c5eda244c6386
Toxic-Whitelgnd <whitelegend56@gmail.com>
2024-10-28 20:35:46 +0530

    disabled unit tests, setting active profile as dev

--- src/test/java/com/tarun/ghee/GheeApplicationTests.java
+++ src/test/java/com/tarun/ghee/GheeApplicationTests.java
@@ -3,8 +3,11 @@ package com.tarun.ghee;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
+@Disabled
 class GheeApplicationTests {
 
 	@Test
